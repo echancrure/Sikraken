1f0c20c03aaaf22547999cfa0da99bc54e16a452
Chris Meudec <echancrure@gmail.com>
2024-07-27 16:47:36 +0100

    adding heap and seav!

--- SymbolicExecutor/se_handle_all_declarations.pl
+++ SymbolicExecutor/se_handle_all_declarations.pl
@@ -4,7 +4,24 @@ symbolic_execute_all_declarations([Declaration|R]) :-
     symbolic_execute_all_declarations(R).
 
 symbolic_execute(declaration(Specifiers, Declarators)) :-
-    .
+    extract_type(Specifiers, Type_name),
+    declare_declarators(Declarators, Type_name).
 
 symbolic_execute(function(Specifiers, Function_name, Params, [], Compound_statement)) :-
-    .
+    .  
+%%%
+extract_type([int], int) :-
+    !.
+extract_type(Specifiers, _Type_name) :-
+    common_util__error(9, "Not Handled", "Sikraken needs expanding", [('Specifiers', Specifiers)], 9270724, 'se_handle_all_declarations', 'extract_type', no_localisation, no_extra_info).
+%%%
+declare_declarators([], _).
+declare_declarators([Declarator|R], Type_name) :-
+    (Declarator = initialised(Var, Expresion) ->
+        (symbolically_interpret(Expression, Symbolic, Constraint, Type),
+         se_heap__create_var(Var, Type_name, Constraint)
+        )
+    ;
+        se_heap__create_var(Var, Type_name, no_init)
+    ).
+%%% 
---
+++ SymbolicExecutor/se_seav.pl
@@ -0,0 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SEAV
+% Defines the module seav for Symbolic Execution Attributed Variables
+%   briefly the attribute is of the form : mika_seav(Name, Address, Input_value, Output_value, Type)
+%                                                    N     A        I            O             T
+%   Name         : name of the variable
+%   Address      : address of the vraiable
+%   Input_value  : the input value of the variable: free for uninitialised variables, immutable during symbolic execution
+%   Output_value : the current value of the varaible: initially Input_value, mutated via assignments
+%   Type         : just the type_name of the variable
+%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+:- module('se_seav').
+:- export se_seav__create_var/3.
