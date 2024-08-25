66c56dfd8624b55888792b714fe9bf7d7992d24e
“Christophe <echancrure@gmail.com>
2024-08-25 11:54:40 +0100

    moving things

--- legacy_regression_tests/get_sign.pl
+++
@@ -1,9 +0,0 @@
-parsed([
-function_definition(LC_two_ifs, [
-declaration(int, [LC_x_4], []) ], [
-if_statement(6, expression(LC_x_4==0), [return(0)], []),
-if_statement(5, expression(LC_x_4>0), [return(1)], []),return((-1))], int),
-
-global_variables(999, 999)
-
-]).
