e8d9ab9187259a0acd3c95bf047fa068243e1f9b
echancrure <echancrure@gmail.com>
2024-08-26 14:36:07 +0100

    adding operators and refactoring C solver

--- SymbolicExecutor/se_coverage.pl
+++ SymbolicExecutor/se_coverage.pl
@@ -5,5 +5,5 @@ se_coverage__bran_is_already_covered(Branch) :-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 se_coverage__arcs_remaining :-
     %get overall arcs: cannot do this until we build CFG
-    se_globals__get_val('covered_bran', Already_covered).
+    se_globals__get_val('covered_bran', _Already_covered).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
