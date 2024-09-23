6879c0ccdeaa0aca93cc7c036b4722e5e2aa248b
echancrure <echancrure@gmail.com>
2024-09-23 12:43:00 +0100

    regression testing with hcd0, regression 601.19'' [too long]

--- bin/run_regression.sh
+++ bin/run_regression.sh
@@ -113,16 +113,19 @@ for regression_test_file in "$c_files_directory"/*.c; do
         echo "Coverage is: $coverage_value"
 
         if [ "$expected_test_inputs_number" != "$test_nb_value" ]; then
-            echo "Error: Tests generation mismatch! Expected: $expected_test_inputs_number, but got: $test_nb_value when running: $eclipse_call."
-            exit 1
+            echo "Error: Tests generation mismatch! Expected: $expected_test_inputs_number, but got: $test_nb_value when running: $eclipse_call." >> regression_tests_run.log
         fi
 
         if [ "$expected_coverage" != "$coverage_value" ]; then
-            echo "Error: Coverage mismatch! Expected: $expected_coverage, but got: $coverage_value when running: $eclipse_call."
-            exit 1
+            echo "Error: Coverage mismatch! Expected: $expected_coverage, but got: $coverage_value when running: $eclipse_call." >> regression_tests_run.log
         fi
 
     done
 done
 
-echo -e "\e[32mSikraken regression testing script run_regression.sh has terminated successfully.\e[0m"
+if [ -f regression_tests_run.log ]; then
+    echo -e "\e[31mSikraken regression testing script run_regression.sh has terminated successfully.\e[0m"
+    cat warnings.log
+else
+    echo -e "\e[32mSikraken regression testing script run_regression.sh has terminated successfully.\e[0m"
+fi
