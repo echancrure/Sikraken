eb9a04df1a665dad6948ab09565642c1225a1c87
echancrure <echancrure@gmail.com>
2024-10-02 12:56:40 +0100

    updating script to parser for no extension on input file

--- bin/call_parser.sh
+++ bin/call_parser.sh
@@ -2,33 +2,33 @@
 
 # Ensure we have at least 2 arguments
 if [ $# -lt 2 ]; then
-    echo "Usage: $0 <directory> <file> [gcc_flag]"
+    echo "Usage: $0 <directory> <file_no_ext> [gcc_flag]"
     exit 1
 fi
 
-input_file="$2"
-output_file="${input_file%.*}.i"
+input_file_no_ext="$2"
+output_file="$input_file.i"
 directory="$1"
 
 # Optional third argument for data_model, default to '-m32' if absent
 gcc_flag="${3:--m32}"
 
 # Preprocess the file using gcc
-gcc -E -P "$directory/$input_file" $gcc_flag -o "$directory/$output_file"
+gcc -E -P "$directory/$input_file_no_ext.c" $gcc_flag -o "$directory/$output_file"
 
 # Check if gcc was successful
 if [ $? -ne 0 ]; then
-    echo "Error: gcc failed to preprocess $input_file"
+    echo "Error: gcc failed on gcc -E -P "$directory/$input_file_no_ext.c" $gcc_flag -o "$directory/$output_file""
     exit 1
 fi
 
 # Run the parser
-./bin/sikraken_parser.exe -d $gcc_flag -p"$directory" "$input_file"
+./bin/sikraken_parser.exe -d $gcc_flag -p"$directory" "$input_file_no_ext"
 
 # Check if sikraken_parser was successful
 if [ $? -ne 0 ]; then
-    echo "Error: sikraken_parser failed on $input_file"
+    echo "Error: sikraken_parser failed on ./bin/sikraken_parser.exe -d $gcc_flag -p"$directory" "$input_file_no_ext""
     exit 1
 fi
 
-echo "Successfully preprocessed $input_file and ran sikraken_parser."
+echo "Successfully preprocessed $input_file_no_ext and ran sikraken_parser."
