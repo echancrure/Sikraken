09cf6ce5484548a7dcff08f467f323acb12c1c9d
echancrure <echancrure@gmail.com>
2024-10-19 06:53:02 +0100

    Added casting expresssions on variable declaration; regeression tests run 294.19''

--- SymbolicExecutor/se_handle_declarations.pl
+++ SymbolicExecutor/se_handle_declarations.pl
@@ -3,26 +3,19 @@ declare_declarators([], _).
 declare_declarators([Declarator|R], Type_name) :-
     %mytrace,
     (nonvar(Declarator), Declarator = initialised(Var, Expression) ->   %added nonvar(...) as a guard 30/07/24
-        symbolically_interpret(Expression, symb(_Type, Symbolic))    %todo: handling initialised variables without re-using assignment symbolic execution is probably a bad idea
+        symbolically_interpret(cast(Type_name, Expression), symb(_Type, Casted))
     ;
         (%declaration of non-initialised variable
          %todo check for redefinition which is allowed: see diary 07/08/24
          Var = Declarator,
-         Symbolic = 0 %todo: should be only for global and static variables as non-static and automatic objects are not initialised in C see K&R p. 219
+         Casted = 0 %todo: should be only for global and static variables as non-static and automatic objects are not initialised in C see K&R p. 219
         )
     ),
+    %C pointers variables are not ptc_solver variable: they are handled syntactically e.g. seav(pointer(integer), not_needed, addr(Y_2{se_seav_atts : seav(integer, not_needed, 42)}))
     extract_pointers(Var, Type_name, Type_name_ptr_opt, Clean_var), %e.g. extract_pointers(pointer(Pi_3{c_id(pi)}), integer, pointer(integer), Pi_3)
     seav__create_var(Type_name_ptr_opt, Clean_var),
     seav__update(Clean_var, 'input', 'not_needed'),
-    %create ptc solver variable and = to Symbolic
-    (Type_name_ptr_opt = pointer(_) ->
-        Output = Symbolic           %C pointers variables are not ptc_solver variable: they are handled syntactically e.g. seav(pointer(integer), not_needed, addr(Y_2{se_seav_atts : seav(integer, not_needed, 42)}))
-    ;
-        (ptc_solver__variable([Output], Type_name),
-         ptc_solver__sdl(Output = Symbolic)     %todo use assignment to respect casting
-        )
-    ),
-    seav__update(Clean_var, 'output', Output),
+    seav__update(Clean_var, 'output', Casted),
     declare_declarators(R, Type_name).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %e.g. extract_pointers(pointer(X), int, pointer(int), X)
