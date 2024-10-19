3cd3d073c3711be00c2873e54d15d9160d5d209a
echancrure <echancrure@gmail.com>
2024-10-19 20:13:41 +0100

    fixed casting to narrower integer type; regression tests 306.47''

--- SampleCode/atry_bitwise.c
+++ SampleCode/atry_bitwise.c
@@ -1,48 +1,50 @@
 extern int __VERIFIER_nondet_int();
+#include <stdio.h>
 int main() {
-    int sample = __VERIFIER_nondet_int();
+ //   int sample = __VERIFIER_nondet_int();
+    int sol = 0;
     // Bitwise AND (&)
     int a = 12;    // 1100 in binary
     int b = 10;    // 1010 in binary
     int and_result = a & b;  // 1000 in binary -> 8
-    if (and_result == 8);
+    if (and_result == 8) sol++;
 
     // Bitwise OR (|)
     int or_result = a | b;   // 1110 in binary -> 14
-    if (or_result == 14);
+    if (or_result == 14) sol++;
 
     // Bitwise XOR (^)
     int xor_result = a ^ b;  // 0110 in binary -> 6
-    if (xor_result == 6);
+    if (xor_result == 6) sol++;
 
     // Bitwise NOT (~)
     unsigned int c = 0x0F;   // 00001111 in binary
     unsigned int not_result = ~c;  // 11110000 in binary (with unsigned) -> 0xFFFFFFF0
-    if (not_result == 0xFFFFFFF0);
+    if (not_result == 0xFFFFFFF0) sol++;
 
     // Bitwise left shift (<<)
     int d = 5;               // 00000101 in binary
     int shift_left = d << 1;  // 00001010 in binary -> 10
-    if (shift_left == 10);
+    if (shift_left == 10) sol++;
 
     // Bitwise right shift (>>)
     int shift_right = d >> 1; // 00000010 in binary -> 2
-    if (shift_right == 2);
+    if (shift_right == 2) sol++;
 
     // Using bitwise operators with `char` type
-    char e = 0x3C;           // 00111100 in binary
+    char e = 0x3C;           // 00111100 in binary, 61 in decimal
     char f = 0x0F;           // 00001111 in binary
     char char_and = e & f;    // 00001100 in binary -> 12
-    if (char_and == 12);
+    if (char_and == 12) sol++;
 
     char char_or = e | f;     // 00111111 in binary -> 63
-    if (char_or == 63);
+    if (char_or == 63) sol++;
 
     char char_xor = e ^ f;    // 00110011 in binary -> 51
-    if (char_xor == 51);
+    if (char_xor == 51) sol++;
 
     char char_not = ~e;       // 11000011 in binary (2's complement) -> -61
-    if (char_not == -61);
-
+    if (char_not == -61) sol++;
+    printf("sol is %d", sol);
     return 0;
 }
--- SampleCode/try_conv.c
+++ SampleCode/try_conv.c
@@ -1,13 +1,31 @@
-#include <stdio.h>
+//#include <stdio.h>
 
 int main() {
 unsigned char uc = 300;
-printf("ui:%hhu\n", uc);
+//printf("ui:%hhu\n", uc);
 
 unsigned char c = 42;
 unsigned char cu = -c;
 int ci = -c;
-printf("minus unsigned char:%hhu\n", cu);
-printf("unary minus to integer:%i\n", ci);
+//printf("minus unsigned char:%hhu\n", cu);
+//printf("unary minus to integer:%i\n", ci);
+
+
+char c1 = 300;
+//printf("c1 is %d\n", c1); // c1 is 44
+char c2 = -300;
+//printf("c2 is %d\n", c2); // c2 is -44
+char c3 = 30;
+//printf("c3 is %d\n", c3); // c3 is 30
+char c4 = -30;
+//printf("c4 is %d\n", c4); // c4 is -30
+char c5 = 255;
+//printf("c5 is %d\n", c5); // c5 is -1
+char c6 = -255;
+//printf("c6 is %d\n", c6); // c6 is 1
+char c7 = 127;
+//printf("c7 is %d\n", c7); // c7 is 127
+char c8 = -127;
+//printf("c8 is %d\n", c8); // c8 is -127
 
 }
