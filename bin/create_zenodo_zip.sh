15eb6dff85183134ba86859c67b325903e2675f5
echancrure <echancrure@gmail.com>
2024-10-30 19:57:49 +0000

    new zenodo archiver under single root

--- bin/create_zenodo_zip.sh
+++ bin/create_zenodo_zip.sh
@@ -4,6 +4,10 @@
 # Define the name of the output zip file
 OUTPUT_ZIP="sikraken.zip"
 
+# Create a temporary root directory
+TEMP_ROOT="my_root"
+mkdir -p "$TEMP_ROOT"
+
 # Define the list of files and folders you want to include in the zip
 FILES_TO_INCLUDE=(
     "./bin"
@@ -23,7 +27,15 @@ EXCLUDE_PATTERNS=(
     "*.project*"
 )
 
-# Create the zip archive
-zip -r $OUTPUT_ZIP "${FILES_TO_INCLUDE[@]}" -x "${EXCLUDE_PATTERNS[@]}"
+# Copy files to the temporary root directory
+for FILE in "${FILES_TO_INCLUDE[@]}"; do
+    cp -r "$FILE" "$TEMP_ROOT/"
+done
+
+# Create the zip archive from the temporary root directory
+ zip -r "$OUTPUT_ZIP" "$TEMP_ROOT" -x "${EXCLUDE_PATTERNS[@]}"
+
+# Clean up by removing the temporary root directory
+rm -rf "$TEMP_ROOT"
 
 echo "Zip archive $OUTPUT_ZIP has been created with the specified files and folders."
