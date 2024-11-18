37c128c65d7ed43d8e1be8d2c6975408ef1c3150
echancrure <echancrure@gmail.com>
2024-11-18 15:51:12 +0000

    starting to handle array declaration

--- SampleCode/atry_array.c
+++ SampleCode/atry_array.c
@@ -1,8 +1,10 @@
 extern int __VERIFIER_nondet_int();
 int main() {
-    int a619562473[] = {44,45,46,47,48,49};
-	int *a523095507 = a619562473;
-    int x;
-    x = a523095507[3] + a619562473[3];
+	int a[] = {44,45,46,47,48,49};
+	int *ap = a;
+    int x = 5;
+    x = ap[3] + a[3];   //97
+    int input = __VERIFIER_nondet_int();
+    if (input == x) ; else ;
     return 0;
 }
