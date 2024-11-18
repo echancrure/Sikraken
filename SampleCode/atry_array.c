08087bd2dfea1d5dcf68bf539d14e541185aeae8
echancrure <echancrure@gmail.com>
2024-11-18 13:01:35 +0000

    new c array example

---
+++ SampleCode/atry_array.c
@@ -0,0 +1,8 @@
+extern int __VERIFIER_nondet_int();
+int main() {
+    int a619562473[] = {44,45,46,47,48,49};
+	int *a523095507 = a619562473;
+    int x;
+    x = a523095507[3] + a619562473[3];
+    return 0;
+}
