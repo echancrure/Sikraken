458c251681ea079ca8dd30845bafb505f50d8536
echancrure <echancrure@gmail.com>
2024-11-20 11:02:40 +0000

    fixed bug in default declaration values ; regression tests 317.95''

---
+++ SampleCode/atry_idiv.c
@@ -0,0 +1,11 @@
+extern int __VERIFIER_nondet_int();
+int main() {
+    int x, y, z;
+    x = __VERIFIER_nondet_int();
+    y = __VERIFIER_nondet_int();
+    z = __VERIFIER_nondet_int();
+    z = x / y;
+    if (z == 42) ;
+    else ;
+    return 0;
+}
--- SampleCode/parsing_IF_error.c
+++
@@ -1,6 +0,0 @@
-int main() {
-    char a = 'a';
-    int x = 42;
-    if ( x == 'a') ; else ;
-    char a = '0';
-}
