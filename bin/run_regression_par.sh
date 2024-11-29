832c2677183d3d0d45416dcaa3740ef9dcf88832
echancrure <echancrure@gmail.com>
2024-11-29 21:05:58 +0000

    Parallel regression testing; regression 310.95 (3:32 wall)

--- bin/run_regression_par.sh
+++ bin/run_regression_par.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 clear
+echo "PARALLEL Regression Testing"
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)" #Get the directory of the script <sikraken_install>/bin
 SIKRAKEN_INSTALL_DIR="$SCRIPT_DIR/.."
-echo "Sikraken regression testing run_regression.sh says: SIKRAKEN_INSTALL_DIR is $SIKRAKEN_INSTALL_DIR"
+echo "SIKRAKEN_INSTALL_DIR is $SIKRAKEN_INSTALL_DIR"
 
-# Check if the directory argument is provided
-if [ -z "$1" ]; then
-    echo "Sikraken regression testing ERROR: script usage is $0 <relative_directory_of_regression_c_files> e.g. regression_tests"
+# Check if the correct number of arguments is provided
+if [ $# -ne 2 ]; then
+    echo "Sikraken PARALLEL regression testing ERROR: script usage is $0 <number_of_cores> <relative_directory_of_regression_c_files> e.g. 4 regression_tests"
     exit 1
 fi
+# Maximum number of concurrent jobs
+max_jobs=$1
 
 # Set the directory containing the .c files from the argument
-rel_path_c_file="$1"
+rel_path_c_file="$2"
 c_files_directory="$SIKRAKEN_INSTALL_DIR/$rel_path_c_file"    #e.g. /home/chris/Sikraken/regression_tests
 
 # Check if the provided directory exists
@@ -34,8 +37,7 @@ if [ -f regression_tests_run.log ]; then
     rm -f regression_tests_run.log
 fi
 
-# Maximum number of concurrent jobs
-max_jobs=4
+
 
 # Job pool to limit the number of background processes
 job_pool() {
@@ -44,8 +46,7 @@ job_pool() {
     done
 }
 
-# Function to process each .c file
-process_file() {
+generate_tests() {
     local regression_test_file="$1"
     
     # Extract the base name of the file (without the path nor extension)
@@ -69,13 +70,11 @@ process_file() {
     fi
 
     # Generate GCC flag based on the value of data_model
-    local gcc_flag testcov_data_model
+    local gcc_flag
     if [ "$data_model" == "ILP32" ]; then
         gcc_flag="-m32"
-        testcov_data_model="-32"
     elif [ "$data_model" == "LP64" ]; then
         gcc_flag="-m64"
-        testcov_data_model="-64"
     else
         echo "Sikraken regression testing ERROR: Unsupported data model: $data_model"
         return 1
@@ -90,19 +89,16 @@ process_file() {
         echo "Sikraken successfully parsed $regression_test_file"
     fi
 
-    # Loop over all configurations in the configuration file
+    # Loop over all configurations in the configuration file (only support one configuration file in parallel mode because the test-suite generated gets overwritten otherwise)
     local config_count=$(jq '.configurations | length' "$config_file")
 
     for i in $(seq 0 $((config_count - 1))); do
         # Extract configuration data
         local config=$(jq ".configurations[$i]" "$config_file")
 
-        local id=$(echo "$config" | jq -r '.id')
         local algo=$(echo "$config" | jq -r '.algo')
-        local expected_test_inputs_number=$(echo "$config" | jq -r '.expected_test_inputs_number')
-        local expected_coverage=$(echo "$config" | jq -r '.expected_coverage')
 
-        echo -e "\e[34mGenerating tests for $regression_test_file in $id configuration.\e[0m"
+        echo -e "\e[34mGenerating tests for $regression_test_file using also: $algo\e[0m"
 
         # Generate test inputs
         local eclipse_call="se_main(['$SIKRAKEN_INSTALL_DIR', '$SIKRAKEN_INSTALL_DIR/$rel_path_c_file', '$base_name', main, debug, testcomp, '$gcc_flag', $algo])"
@@ -114,19 +110,58 @@ process_file() {
             echo "Test inputs generated for $regression_test_file in $id configuration"
         fi
 
-        # Validate test inputs
-        # local testcov_output=$($SIKRAKEN_INSTALL_DIR/bin/run_testcov.sh "$regression_test_file" "$testcov_data_model")
-        zipfile="./sikraken_output/$base_name/test-suite.zip"
+    done
+}
+
+call_testcov() {
+    local regression_test_file="$1"
+    echo "calling test cov for $regression_test_file"
+        
+    # Extract the base name of the file (without the path nor extension)
+    local base_name=$(basename "$regression_test_file" .c)
+    local config_file="$c_files_directory"/"$base_name".json
+    local yml_file="$c_files_directory"/"$base_name".yml
+
+    # Check if the testcomp yml file exists
+    local data_model
+    if [ ! -f "$yml_file" ]; then
+        echo "Sikraken regression testing WARNING: .yml file $yml_file does not exist, assuming ILP32"
+        data_model="ILP32"
+    else
+        data_model=$(grep "data_model:" "$yml_file" | awk '{print $2}')
+    fi
+
+    # Generate GCC flag based on the value of data_model
+    local testcov_data_model
+    if [ "$data_model" == "ILP32" ]; then
+        testcov_data_model="-32"
+    elif [ "$data_model" == "LP64" ]; then
+        testcov_data_model="-64"
+    fi
+
+    # Loop over all configurations in the configuration file
+    local config_count=$(jq '.configurations | length' "$config_file")
+
+    for i in $(seq 0 $((config_count - 1))); do
+        # Extract configuration data
+        local config=$(jq ".configurations[$i]" "$config_file")
+
+        local expected_test_inputs_number=$(echo "$config" | jq -r '.expected_test_inputs_number')
+        local expected_coverage=$(echo "$config" | jq -r '.expected_coverage')
+
+        local zipfile="./sikraken_output/$base_name/test-suite.zip"
         rm -f $zipfile
         zip -r $zipfile "./sikraken_output/$base_name/test-suite"
-        local testcov_output=$(testcov --no-isolation $testcov_data_model --test-suite $zipfile $rel_path_c_file --output "./sikraken_output/$base_name/testcov" --reduction NONE)
-        #local testcov_output=$($SIKRAKEN_INSTALL_DIR/bin/run_testcov.sh "$regression_test_file" "$testcov_data_model")
+
+        local testcov_call="testcov $testcov_data_model --test-suite $zipfile $regression_test_file --output "./sikraken_output/$base_name/testcov" --reduction NONE --no-runexec --no-isolation --no-plots"
+        echo "CALL TO TESTCOV IS $testcov_call"
+        local testcov_output=$($testcov_call 2>&1)
         if [ $? -ne 0 ]; then
             echo "ERROR: TestCov validation failed for $regression_test_file"
             return 1
         fi
 
-        # Process and check results
+        # get the number of test run and coverage achieved from testcov output
         local tests_nb_line=$(echo "$testcov_output" | grep "Tests run:")
         local test_nb_value=$(echo "$tests_nb_line" | awk '{print $3}')
         local coverage_line=$(echo "$testcov_output" | grep "Coverage:")
@@ -134,24 +169,30 @@ process_file() {
 
         echo "Tests: $test_nb_value ($expected_test_inputs_number expected), Coverage: $coverage_value% ($expected_coverage% expected)"
 
-        if [ "$expected_test_inputs_number" != "$test_nb_value" ]; then
-            echo "Error: Test count mismatch for $regression_test_file in $id configuration." >> regression_tests_run.log
+        if [ $expected_test_inputs_number != $test_nb_value ]; then
+            echo "Error: Tests generation mismatch! For $regression_test_file in $id configuration, expected: $expected_test_inputs_number, but got: $test_nb_value." >> regression_tests_run.log
         fi
 
         if [ "$expected_coverage" != "$coverage_value" ]; then
-            echo "Error: Coverage mismatch for $regression_test_file in $id configuration." >> regression_tests_run.log
+            echo "Error: Coverage mismatch! For $regression_test_file in $id configuration, expected: $expected_coverage, but got: $coverage_value." >> regression_tests_run.log
         fi
     done
 }
+# main
 
 # Loop over all .c files and process them in parallel
 for regression_test_file in "$c_files_directory"/*.c; do
     job_pool  # Wait for an available slot
-    process_file "$regression_test_file" &  # Run in the background
+    generate_tests "$regression_test_file" &  # Run in the background
 done
 
 # Wait for all background jobs to finish
 wait
+
+# Run testcov sequentially [because I can't find a way to make it run in parallel even in so-called isolation mode and/or runexec]
+for regression_test_file in "$c_files_directory"/*.c; do
+    call_testcov "$regression_test_file"
+done
 
 if [ -f regression_tests_run.log ]; then
     echo -e "\e[31mSikraken regression testing script run_regression.sh has terminated with warnings.\e[0m"
