cde7555f7ea22843a6dd10e2bf9293092ae7d035
S <syedmumer15071@gmail.com>
2024-12-29 06:58:36 +0000

    Deleted unwanted files from git

--- Parser/output.pl
+++
@@ -1,7 +0,0 @@
-prolog_c([function([int], function(Main, []), [], 
-cmp_stmts([
-declaration([typedef, union('anonymous', [struct_decl([struct(__pthread_rwlock_arch_t, 'forward')], [UC___data]), struct_decl([char], [array_decl(UC___size, int(56))]), struct_decl([long], [UC___align])])], [Pthread_rwlock_t]), 
-return_stmt(int(0))
-
-]))
-]).
