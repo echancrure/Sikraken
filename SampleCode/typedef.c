46f67d01f504bbdee1f8217c4d9c53192b9f9292
echancrure <echancrure@gmail.com>
2025-02-04 16:49:07 +0000

    typedef.c example added

---
+++ SampleCode/typedef.c
@@ -0,0 +1,22 @@
+struct table;   //struct table is declared
+typedef struct table table; //typedef is used to define table as struct table
+union anonymous_62146 {
+    int *table;  // table is a member, just an identifier
+    int pat;
+};
+struct table {  //struct table is defined
+    const char *name;
+    int type;
+    int value;
+};
+static const table universal_time_zone_table_200015[4]; //table is a typedef name denoting struct table
+
+int hello(int *table) { //table is a parameter, an identifier, not a typedef name
+    return 0;
+}
+
+int main() {
+    int table = 5; //table is a variable, not a typedef name
+    int x = 6 + table; //table is a variable, not a typedef name
+    return x;
+}
