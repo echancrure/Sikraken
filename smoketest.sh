ad49b0bf2b926adfb06e10684a5fd9f08097dabf
echancrure <echancrure@gmail.com>
2025-11-04 18:38:46 +0000

    fix smoketest script

--- smoketest.sh
+++ smoketest.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)" #Get the directory of the script <sikraken_install>/bin
 "$SCRIPT_DIR/bin/sikraken.sh" release budget[1] -m32 SampleCode/simple_if.c
